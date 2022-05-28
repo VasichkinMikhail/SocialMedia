31c09b760baa78c21b343e2bb83f267396e4154d
mikhail <budarain@mail.ru>
2022-05-28 15:32:05 +0300

    Использован паттерн Builder в построение контента. Использован паттерн Decorator в публикации контента парралельно в Facebook и Twitter. Использован паттерн Observe сообщающий подписчикам что появидся новый контент. Использован Data Mapper  и Identity Mapper.

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -2,47 +2,53 @@ package ru.geekbrains;
 
 import org.springframework.context.annotation.Bean;
 import ru.geekbrains.builder.ContentBuilder;
+import ru.geekbrains.dataMaper.Registry;
+import ru.geekbrains.dataMaper.UserMapper;
 import ru.geekbrains.model.Person;
 import ru.geekbrains.model.TextContent;
+import ru.geekbrains.model.User;
 import ru.geekbrains.observe.Bot;
+
+
 
 
 public class Main {
     public static void main(String[] args) {
 
-        TextContent content = new ContentBuilder()
-                .body("Hi! It`s my first publication. ")
-                .theme("My first text")
-                .build();
-        Notifier notifier = getNotifier();
-        notifier.send(content);
+        UserMapper userMapper = Registry.getUserMapper();
 
-        Channel channel = new Channel("Mike channel");
-        Person mike = new Person("Mike");
-        Bot bot = new Bot();
-        channel.subscribe(mike);
-        channel.subscribe(bot);
-        channel.addContent(content);
+        User user = userMapper.find(1L);
+        System.out.println("user "+user.getName());
+
+        User user1 = userMapper.find(1L);
+        System.out.println(" user1"+ user1.getName());
 
 
 
-
-
-
-
-//        User user = User.builder()
-//                .id(1L)
-//                .userName("Bob")
-//                .phoneNumber("89099990099")
-//                .email("bob@gmail.com")
+//        TextContent content = new ContentBuilder()
+//                .body("Hi! It`s my first publication. ")
+//                .theme("My first text")
 //                .build();
+//        Notifier notifier = getNotifier();
+//        notifier.send(content);
+//
+//        Channel channel = new Channel("Mike channel");
+//        Person mike = new Person("Mike");
+//        Bot bot = new Bot();
+//        channel.subscribe(mike);
+//        channel.subscribe(bot);
+//        channel.addContent(content);
 
 
 
-//        System.out.println(content.toString());
+
+
+
+
+
     }
-    @Bean
-    private static Notifier getNotifier() {
-        return new TwiterNotifier(new FaceBookNotifier(new BaseNotifier()));
-    }
+//    @Bean
+//    private static Notifier getNotifier() {
+//        return new TwiterNotifier(new FaceBookNotifier(new BaseNotifier()));
+//    }
 }
